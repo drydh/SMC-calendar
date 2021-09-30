d85894e5aa0ae4c07f801c79d0248db2a2161cd2
David Rydh <dary@math.kth.se>
2021-09-30 09:53:37 +0200

    convert-tex-to-polopoly.sh: Understand italic.

--- convert-tex-to-polopoly.sh
+++ convert-tex-to-polopoly.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 # Usage: convert-tex-to-polopoly.sh in.tex > out.html
 
-sed -e 's/\$\([^$]*\)\$/<span\ class="math-tex">\\(\1\\)<\/span>/g' "$@"
+sed -e 's/\$\([^$]*\)\$/<span\ class="math-tex">\\(\1\\)<\/span>/g' \
+	-e 's/\\emph{\([^}]*\)}/<i>\1<\/i>/g' \
+	-e 's/\\textit{\([^}]*\)}/<i>\1<\/i>/g' \
+	"$@"
 
 # TODO:
-# Convert \emph{...} -> <i>...</i>
 # Remove single line breaks
 # Convert double line break to <p>...</p>
